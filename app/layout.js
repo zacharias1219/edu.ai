beaf12a5ae7568c1d9082e31b7261f2c9418fa17
Richard Abishai <rickoshade1891@gmail.com>
2025-03-25 22:08:47 +0530

    add StackFrame integration with user authentication and loading states

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { StackProvider, StackTheme } from "@stackframe/stack";
+import { stackServerApp } from "../stack";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -21,9 +23,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><StackProvider app={stackServerApp}><StackTheme>
         {children}
-      </body>
+      </StackTheme></StackProvider></body>
     </html>
   );
 }
