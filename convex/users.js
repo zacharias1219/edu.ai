ca1a452ef871ffa56402c8bd3fcbf428750cda42
Richard Abishai <rickoshade1891@gmail.com>
2025-03-26 21:50:52 +0530

    add user creation mutation and integrate users module in API

---
+++ convex/users.js
@@ -0,0 +1,24 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+export const createUser=mutation({
+    args: {
+        name:v.string(),
+        email:v.string()
+    },
+    handle:async(convexToJson, args) => {
+        const userData = await ctx.db.query('users')
+        .filter(q => q.eq(q.field('email'), args.email))
+        .collect();
+        if(userData?.length==0){
+            const data={
+                name:args.name,
+                email:args.email,
+                credits:50000
+            }
+            const result = await ctx.db.insert('users', {
+                
+            })
+        }
+    }
+})
