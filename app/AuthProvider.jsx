dc7533350607c96bc1e6a9ad0730b796f747f36f
Richard Abishai <rickoshade1891@gmail.com>
2025-03-29 21:32:47 +0530

    add user authentication handling in AuthProvider; integrate user creation on login and enhance loading state in Provider

--- app/AuthProvider.jsx
+++ app/AuthProvider.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+"use client"
+import { useUser } from '@stackframe/stack'
+import { useMutation } from 'convex/react'
+import { api } from "@/convex/_generated/api"
+import React, { useEffect } from 'react'
 
 function AuthProvider({children}) {
+  const user = useUser();
+  const CreateUser = useMutation(api.users.CreateUser);
+
+  useEffect(() => {
+      if (!user) {
+        console.log(user)
+        user && CreateNewUser();
+      }
+  }, [user])
+
+  const CreateNewUser = async () => {
+    const result = await CreateUser({
+      name:user?.displayName,
+      email:user.primaryEmail
+    });
+    console.log(result);
+  }
   return (
     <div>
         {children}
--- app/provider.js
+++ app/provider.js
@@ -1,18 +1,18 @@
 "use client"
-import React from 'react'
+import React, { Suspense } from 'react'
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import AuthProvider from './AuthProvider';
 
 function Provider({children}) {
   const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
   return (
-    <div>
+    <Suspense fallback={<p>Loading...</p>}>
         <ConvexProvider client={convex}>
           <AuthProvider>
             { children }
           </AuthProvider>
         </ConvexProvider>
-    </div>
+    </Suspense>
   )
 }
 
