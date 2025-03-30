26b92660d9256364eab7808f9bd8e3f1385c24b5
Richard Abishai <rickoshade1891@gmail.com>
2025-03-30 21:00:47 +0530

    add Feedback and History components; integrate them into Dashboard layout

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import FeatureAssistance from './_components/FeatureAssistance'
+import History from './_components/History'
+import Feedback from './_components/Feedback'
 
 function Dashboard() {
   return (
     <div>
         <FeatureAssistance />
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-10 mt-10'>
+            <History />
+            <Feedback />
+        </div>
     </div>
   )
 }
