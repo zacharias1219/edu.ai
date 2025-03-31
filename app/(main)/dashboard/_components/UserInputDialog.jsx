9868b65231de050b8495155c2bfe6e70dc6cb065
Richard Abishai <rickoshade1891@gmail.com>
2025-03-31 18:15:41 +0530

    enhance UserInputDialog with expert selection; add state management for selected expert and improve UI interactions

--- app/(main)/dashboard/_components/UserInputDialog.jsx
+++ app/(main)/dashboard/_components/UserInputDialog.jsx
@@ -12,7 +12,9 @@ import { CoachingExpert } from '@/services/Options';
 import { View } from 'lucide-react';
 
 function UserInputDialog({children, coachingOption}) {
-  return (
+    const [selectedExpert, setSelectedExpert] = React.useState();
+
+    return (
     <Dialog>
         <DialogTrigger>
             {children}
@@ -24,11 +26,11 @@ function UserInputDialog({children, coachingOption}) {
                     <div className='mt-3'>
                         <h2 className='text-black'>Enter a topic to master yout skills in {coachingOption.name}</h2>
                         <Textarea placeholder='Enter a topic' className="mt-2"/>
-                        <div className='grid grid-col-3md:grid-col-5 gap-6'>
+                        <div className='grid grid-col-3md:grid-col-5 gap-6 mt-3'>
                             {CoachingExpert.map((expert, index) => (
-                                <div key={index}>
-                                    <Image src={expert.avatar} alt={expert.name} width={100} height={100} className="rounded-2xl h-[80px] w-[80px] object-cover"/>
-                                    <h2>{expert.name}</h2>
+                                <div key={index} onCLick={() => setSelectedExpert(expert.name)}>
+                                    <Image src={expert.avatar} alt={expert.name} width={100} height={100} className={`rounded-2xl h-[80px] w-[80px] object-cover hover:scale-105 transition-all cursor-pointer ${selectedExpert==expert.name&&'border-2'} p-1 rounded-2xl`}/>
+                                    <h2 className='text-center '>{expert.name}</h2>
                                 </div>
                             ))}
                         </div>
